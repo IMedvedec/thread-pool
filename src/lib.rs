39f03225b077bdfcd7a733f81502c9eb6c669a95
imedvedec <ivan.medvedec0@gmail.com>
2021-11-01 22:22:37 +0100

    Static lifetime for jobs and additional test

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ struct Worker {
     thread: thread::JoinHandle<()>,
 }
 
-type Job = Box<FnBox + Send>;
+type Job = Box<FnBox + Send + 'static>;
 
 trait FnBox {
     fn call_box(self: Box<Self>);
@@ -25,7 +25,7 @@ impl<F: FnOnce()> FnBox for F {
 
 impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
-        let thread = thread::spawn(move || {
+        let thread = thread::spawn(move || loop {
             let job = receiver.lock().unwrap().recv().unwrap();
 
             println!("Worker #{} got a job; executing!", id);
@@ -53,9 +53,11 @@ impl ThreadPool {
 
     pub fn execute<F>(&self, f: F)
     where
-        F: FnOnce() + Send,
+        F: FnOnce() + Send + 'static,
     {
-        f()
+        let job = Box::new(f);
+
+        self.sender.send(job).unwrap();
     }
 }
 
@@ -78,5 +80,15 @@ mod tests {
     fn execute_one_success() {
         let tp = ThreadPool::new(1);
         tp.execute(|| println!("Hello world!"))
+    }
+
+    #[test]
+    fn execute_five_success() {
+        let tp = ThreadPool::new(3);
+        tp.execute(|| println!("First (#1) job done!"));
+        tp.execute(|| println!("Second (#2) job done!"));
+        tp.execute(|| println!("Third (#3) job done!"));
+        tp.execute(|| println!("Fourth (#4) job done!"));
+        tp.execute(|| println!("Fifth (#5) job done!"));
     }
 }
