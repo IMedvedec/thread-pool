837e36fc5b19f01c86b4d680e90412c7e7071903
imedvedec <ivan.medvedec0@gmail.com>
2021-11-01 21:57:39 +0100

    Multiple consumers take jobs from given channel

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
+use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
 
 struct ThreadPool {
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
 
 struct Worker {
@@ -9,12 +11,28 @@ struct Worker {
     thread: thread::JoinHandle<()>,
 }
 
+type Job = Box<FnBox + Send>;
+
+trait FnBox {
+    fn call_box(self: Box<Self>);
+}
+
+impl<F: FnOnce()> FnBox for F {
+    fn call_box(self: Box<F>) {
+        (*self)()
+    }
+}
+
 impl Worker {
-    fn new(id: usize) -> Worker {
-        Worker {
-            id: id,
-            thread: thread::spawn(|| {}),
-        }
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || {
+            let job = receiver.lock().unwrap().recv().unwrap();
+
+            println!("Worker #{} got a job; executing!", id);
+            job.call_box()
+        });
+
+        Worker { id, thread }
     }
 }
 
@@ -22,17 +40,20 @@ impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+
         let mut workers = Vec::with_capacity(size);
         for id in 0..size {
-            workers.push(Worker::new(id))
+            workers.push(Worker::new(id, Arc::clone(&receiver)))
         }
 
-        ThreadPool { workers: workers }
+        ThreadPool { workers, sender }
     }
 
     pub fn execute<F>(&self, f: F)
     where
-        F: FnOnce(),
+        F: FnOnce() + Send,
     {
         f()
     }
